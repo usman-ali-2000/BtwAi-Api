1b8d3f682f51e643956ea1a472c53f3edece568d
usman-ali-2000 <usmanzulfiqar14@gmail.com>
2025-03-06 12:31:01 +0500

    update cron

--- components/api/cron.js
+++ components/api/cron.js
@@ -1,23 +1,3 @@
-export default async function handler(req, res) {
-    try {
-        // Example PATCH request using Fetch without parameters
-        const response = await fetch('https://btw-ai-api.vercel.app/add-coin', {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log('Patch request successful:', data);
-        res.status(200).json({ message: 'Patch request completed successfully' });
-
-    } catch (error) {
-        console.error('Error during patch request:', error.message);
-        res.status(500).json({ message: 'Patch request failed' });
-    }
-}
+export default function handler(req, res) {
+    res.status(200).end('Hello Cron!');
+  }
